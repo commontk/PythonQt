af8203f10ed914a317062c98dd11b817e72a5274
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2013-09-10 02:49:23 -0400

    Revert part of r91 preventing QFlags from being wrapped
    By checking if an enum member has already been cached it is not required
    anymore to skip it if is a QFlags.
    
    Additionally, the wrapping of QFlags can now be done by using
    only Q_FLAGS without having a corresponding Q_ENUMS.

--- src/PythonQtClassInfo.cpp
+++ src/PythonQtClassInfo.cpp
@@ -273,9 +273,12 @@ bool PythonQtClassInfo::lookForEnumAndCache(const QMetaObject* meta, const char*
   int enumCount = meta->enumeratorCount();
   for (int i=0;i<enumCount; i++) {
     QMetaEnum e = meta->enumerator(i);
-    // we do not want flags, they will cause our values to appear two times
-    if (e.isFlag()) continue;
-    
+    if (_cachedMembers.contains(memberName)) {
+  #ifdef PYTHONQT_DEBUG
+      std::cout << "cached enum " << memberName << " on " << meta->className()  << std::endl;
+  #endif
+      continue;
+      }
     for (int j=0; j < e.keyCount(); j++) {
       if (qstrcmp(e.key(j), memberName)==0) {
         PyObject* enumType = findEnumWrapper(e.name());
@@ -536,9 +539,6 @@ QStringList PythonQtClassInfo::memberList()
     for (int i = 0; i<meta->enumeratorCount(); i++) {
       QMetaEnum e = meta->enumerator(i);
       l << e.name();
-      // we do not want flags, they will cause our values to appear two times
-      if (e.isFlag()) continue;
-
       for (int j=0; j < e.keyCount(); j++) {
         l << QString(e.key(j));
       }
