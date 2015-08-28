5e97dd85f48b493e3b0b86eaadf971ea379b8d77
Max Smolens <max.smolens@kitware.com>
2015-08-28 14:16:08 -0400

    [commontk] Add missing refcount decrements when creating wrappers
    PyObject_GetAttrString returns a new reference. PyDict_SetItemString
    does not steal a reference, so Py_DECREF should be called after
    PyDict_SetItemString.

--- src/PythonQt.cpp
+++ src/PythonQt.cpp
@@ -828,6 +828,7 @@ PythonQtClassWrapper* PythonQtPrivate::createNewPythonQtClassWrapper(PythonQtCla
   // create the new type object by calling the type
   result = (PythonQtClassWrapper *)PyObject_Call((PyObject *)&PythonQtClassWrapper_Type, args, nullptr);
 
+  Py_DECREF(moduleName);
   Py_DECREF(baseClasses);
   Py_DECREF(typeDict);
   Py_DECREF(moduleName);
@@ -863,6 +864,7 @@ PyObject* PythonQtPrivate::createNewPythonQtEnumWrapper(const char* enumName, Py
   // create the new int derived type object by calling the core type
   result = PyObject_Call((PyObject *)&PyType_Type, args, nullptr);
 
+  Py_DECREF(module);
   Py_DECREF(baseClasses);
   Py_DECREF(module);
   Py_DECREF(typeDict);
