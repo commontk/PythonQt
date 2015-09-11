039d785275ff90fc7c44899c45743d4c0a76840f
Max Smolens <max.smolens@kitware.com>
2015-09-11 09:17:59 -0400

    Prevent crashes during and after cleanup
    This commit prevents crashes by handling scenarios such as:
    (a) object destruction after the Python interpreter has been finalized
    (b) object destruction after cleanup, i.e. the singleton no longer exists
    
    Any usage of a Qt enum demonstrates (a).
    
    One example that demonstrates (b) is a QTimer object which is created with a
    QApplication parent. PythonQt::cleanup() is called before the QApplication is
    completely destroyed, so the code that handles wrapping the QTimer object must
    handle the case when the PythonQt singleton no longer exists.
    
    Co-authored-by: Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>

--- src/PythonQtSignalReceiver.cpp
+++ src/PythonQtSignalReceiver.cpp
@@ -172,7 +172,9 @@ PythonQtSignalReceiver::PythonQtSignalReceiver(QObject* obj):PythonQtSignalRecei
 
 PythonQtSignalReceiver::~PythonQtSignalReceiver()
 {
-  PythonQt::priv()->removeSignalEmitter(_obj);
+  if (PythonQt::priv()) {
+    PythonQt::priv()->removeSignalEmitter(_obj);
+  }
 }
 
 
