7cac1bbbb829402d3924b1e52c52a4b4ddbbe9be
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2025-10-23 21:37:39 -0400

    [commontk] cmake: Simplify PythonQt_QtAll reusing PYTHONQT_EXPORT export macro

--- extensions/PythonQt_QtAll/PythonQt_QtAll.cpp
+++ extensions/PythonQt_QtAll/PythonQt_QtAll.cpp
@@ -77,7 +77,7 @@ void PythonQt_init_QtWebKit(PyObject*);
 
 namespace PythonQt_QtAll
 {
-  PYTHONQT_QTALL_EXPORT void init() {
+  PYTHONQT_EXPORT void init() {
 #ifdef PYTHONQT_WITH_CORE
     PythonQt_init_QtCore(0);
 #endif
